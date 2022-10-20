03c8931c2dfb15fccfbf42592112b5926b43a55d
Isabela de Oliveira Athayde <100873483+IsabelaAthayde@users.noreply.github.com>
2022-10-20 03:24:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,77 +1,142 @@
-const body = document.querySelector('body')
+const keys = document.querySelector('#keys')
 const display = document.querySelector('h1')
 const upperDisplay = document.querySelector('span')
 const btnNum = document.querySelectorAll('.numbers')
 const simbols = document.querySelectorAll('.simbols')
 const equalTo = document.getElementById('equalTo')
 const reset = document.getElementById('reset')
+const keyDot = document.querySelector('#dot')
 let leftNum = [];
 let rightNum = [];
 let simb = "";
+let lastNumber = "";
+let lastSimb = "";
 
-body.addEventListener("click", (item) => {
+keys.addEventListener("click", (item) => {
     let clicked = item.target.innerHTML
-    
     if (item.target.classList.contains('simbols')) {
         simb = clicked
     }
 
-    if (item.target.classList.contains('numbers')) {
-        if(simb != "") {
-            rightNum.push(clicked)
+    
+    if(clicked != equalTo) {
+        if (item.target.classList.contains('numbers')) {
             
-        } else {
-           leftNum.push(clicked) 
-           
+            if(simb != "") {
+                rightNum.push(clicked)
+                
+            } else {
+            leftNum.push(clicked) 
+            
+            }
+            
         }
-        
+    }
+
+    if(clicked == "0" && display.textContent == "0") {
+        leftNum[0] = "0"
+        leftNum[1] = "."
+    }
+
+    if(leftNum[0] == ".") {
+        leftNum[0] = "0"
+        leftNum[1] = "."
+    }
+    
+    if(leftNum.includes(".") && simb === "") {
+        keyDot.classList.add("disable")
+    } else {
+        keyDot.classList.remove("disable")
+    }
+
+    if(lastNumber != "" && simb != "") {
+        leftNum = [];
+        rightNum = [];
+        leftNum.push(lastNumber)
+        lastNumber = ""; 
     }
 
     
-
-     let firstValue = parseFloat(leftNum.join(''));
-     let secondValue = parseFloat(rightNum.join(''));
+    let firstValue = leftNum.join('');
+    let secondValue = rightNum.join('');
     
-
+    
+    
+    
     if(simb !== "") {
-        upperDisplay.textContent = firstValue
-        if(secondValue === NaN) {
-            display.textContent = simb + 0
-        } else {
-            display.textContent = simb + secondValue
+        if(firstValue === "") {
+            firstValue = "0"
         }
+        upperDisplay.textContent = firstValue
+        display.textContent = simb + secondValue
         
     } else {
         display.textContent = firstValue
     }
 
-     
-     
-     equalTo.addEventListener('click', function() {
-       
-        display.textContent = calculate(firstValue, simb, secondValue)
-     }) 
+    
 
-     reset.addEventListener('click', function() {
-        leftNum = [];
-        rightNum = [];
-        simb = ""; 
-        display.textContent = "0";
-        upperDisplay.textContent = "  ";
-     })
-})
+    if(clicked == "=") {
+        if(lastNumber != "") {
+            
+            firstValue = lastNumber
+            
+            calculate(firstValue, lastSimb, secondValue)
+            
+         } else {
+           calculate(firstValue, simb, secondValue)
+        }
+        
+    } 
+    
+    function calculate(n1, operator, n2) {
 
-function calculate(n1, operator, n2) {
-    let result = 0;
-    if(operator === "x") {
-        result = n1 * n2
-    } else if(operator === "+") {
-        result = n1 + n2
-    } else if(operator === "-") {
-        result = n1 - n2
-    } else if(operator === "/") {
-        result = n1 / n2
+        if(operator == "x") {
+            var result = parseFloat(n1) * parseFloat(n2)
+        } else if(operator == "+") {
+            var result = parseFloat(n1) + parseFloat(n2)
+        } else if(operator == "-") {
+            var result = parseFloat(n1) - parseFloat(n2)
+        } else if(operator == "/") {
+            var result = parseFloat(n1) / parseFloat(n2)   
+        }
+
+        if (simb != "") {
+            lastSimb = simb 
+            if(firstValue == "0" || result == "0") {
+                if(simb == "+") {
+                    display.textContent = secondValue
+                } else {
+                    display.textContent = lastSimb + secondValue
+                }
+            
+            }
+        }
+        lastNumber = result.toString()
+        upperDisplay.textContent = ""
+        display.textContent = result
+        
+        
+        simb = ""
+    } 
+    
+    if(clicked == "C") { 
+         function reset() { 
+                leftNum = [];
+                rightNum = [];
+                lastNumber = "";
+                display.innerText = "0";
+                simb = ""; 
+                upperDisplay.innerText = "";
+                lastSimb = "";
+                keyDot.classList.remove("disable")
+         }
+        reset()
     }
     
-    return result
-}
+
+     
+})
+
+
+
